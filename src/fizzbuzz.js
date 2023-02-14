17b8dcde44b3555c8443ccd8f126d09f9d3c91ef
Jhonattanr <camicasii@hotmail.com>
2023-02-14 16:13:55 -0500

    refactor

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,7 +1,13 @@
 export default function fizzbuzz(n) {
     if(typeof n !== 'number') throw new Error('n should be a number')
     if(Number.isNaN(n)) throw new Error('n should be a number')
-    if(n % 3 === 0) return 'Fizz'
-
-    return n;
+    const multiplies ={
+        3: 'Fizz',
+        5: 'Buzz'
+    }
+    let output = '';
+    Object.entries(multiplies).forEach(([key, value]) => {
+        if(n % key === 0) output += value;
+    })
+    return output ===''?n:output;
 }
