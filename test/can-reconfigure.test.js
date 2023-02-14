7e1a4080b19dd4f7ddce8680053fbb2424a345fb
Jhonattanr <camicasii@hotmail.com>
2023-02-14 16:35:04 -0500

    new test

--- test/can-reconfigure.test.js
+++ test/can-reconfigure.test.js
@@ -1,6 +1,8 @@
 import { describe, expect, it } from "vitest";
 const canReconfigure =(from, to) => {
     if(typeof from !== "string" || typeof to !== "string") throw new Error("from and to should be strings");
+    if(from.length !== to.length) throw new Error("from and to should be the same length");
+    
 
 
 
@@ -13,6 +15,9 @@ describe("canReconfigure", () => {
     });
     it("should throw an error if from is not a string", () => {
         expect(() => canReconfigure()).toThrow();
+    });
+    if("should thorw if canReconfigure is diferent to length", () => {
+        expect(() => canReconfigure("abc", "ab")).toThrow();        
     });
 
 })
